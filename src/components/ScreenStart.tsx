53596f6b26b24fd8070ed7ac8fb892a9436f1c30
jaylinsg <jstarlin@bu.edu>
2025-08-05 10:48:26 -0400

    guess table

--- src/components/ScreenStart.tsx
+++ src/components/ScreenStart.tsx
@@ -7,7 +7,7 @@ interface StartScreenProps {
 const StartScreen: React.FC<StartScreenProps> = ({ onStart }) => (
     <div className="marauders-container">
         <div className="main-container">
-            <h1 className="start-title">Harry Potter Guessing Game</h1>
+            <h1 className="start-text">Harry Potter Guessing Game</h1>
             <h2>Guess the mystery character in 5 tries!</h2>
             <button className='play-button' onClick={onStart}>Play</button>
         </div>
